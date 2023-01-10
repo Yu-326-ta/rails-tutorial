05ea2f746482961966b998545acc40e97d320585
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-01-10 15:15:19 +0900

    ログアウト機能の実装

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,14 @@ require "rails/test_help"
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+
   fixtures :all
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
