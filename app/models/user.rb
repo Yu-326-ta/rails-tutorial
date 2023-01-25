39028bb7080eba17e659318426b64d0b420ced10
Yu-326-ta <yoshinaga.yuta@moneyforward.co.jp>
2023-01-25 16:27:38 +0900

    インデントの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,7 +91,7 @@ class User < ApplicationRecord
     Micropost.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
              .includes(:user, image_attachment: :blob)
-    end
+  end
 
   # ユーザーをフォローする
   def follow(other_user)
